4f465989b3af2a8be75c434454fb33682af676ee
Abhishek Verma <abhishekverma@192.168.1.2>
2026-01-02 01:30:53 +0530

    Backend handler added for join room, code sync working fine

--- api-gateway/src/realtime/room.gateway.ts
+++ api-gateway/src/realtime/room.gateway.ts
@@ -7,8 +7,7 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'http';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway({
   cors: { origin: '*' },
@@ -16,11 +15,39 @@ import { Socket } from 'socket.io';
 export class RoomGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
+  private rooms = new Map<string, { code: string }>();
   handleConnection(client: Socket) {
     console.log('Client connected: ', client.id);
   }
   handleDisconnect(client: Socket) {
-    console.log('Client connected: ', client.id);
+    console.log('Client disconnected: ', client.id);
+  }
+  @SubscribeMessage('join_room')
+  async handleJoinRoom(
+    @MessageBody() data: { roomId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    const { roomId } = data;
+    await client.join(roomId);
+    const room = this.rooms.get(roomId);
+    client.emit('room_state', {
+      code: room?.code ?? '',
+    });
+  }
+  @SubscribeMessage('code_change')
+  handleMessage(
+    @MessageBody() data: { roomId: string; code: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    // client.emit('code');
+    const { roomId, code } = data;
+    const room = this.rooms.get(roomId) ?? { code: '' };
+    room.code = code;
+    this.rooms.set(roomId, room);
+
+    client.to(roomId).emit('code_change', {
+      code,
+    });
   }
   @SubscribeMessage('ping')
   handlePing(
