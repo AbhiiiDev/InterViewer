622ec186ff93f4b63e661135e61a99f0cd5d63ea
Abhishek Verma <abhishekverma@192.168.1.12>
2026-01-19 23:43:02 +0530

    Fixed app module error of : Interview Service not available

--- api-gateway/src/app.module.ts
+++ api-gateway/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomGateway } from './realtime/room.gateway';
 import { PrismaService } from './prisma/prisma.service';
+import { InterviewModule } from './interview/interview.module';
 
 @Module({
-  imports: [],
+  imports: [InterviewModule],
   controllers: [AppController],
   providers: [AppService, RoomGateway, PrismaService],
 })
